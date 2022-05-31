6e1942a05464ab76f93d0af7b358c7eac8d156c6
Paul Nyishar <paulnyishar@gmail.com>
2022-05-31 02:30:49 +0100

    Swagger configurations added

---
+++ src/main/java/com/pantech/blog/config/SwaggerConfig.java
@@ -0,0 +1,64 @@
+package com.pantech.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Paul Nyishar
+ * @Date 19/05/2022
+ */
+@Configuration
+public class SwaggerConfig {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private ApiKey apiKey(){
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+                "Spring Boot Blog Rest APIs",
+                "Spring Boot Blog Rest API Documentation",
+                "1",
+                "Terms of Service",
+                new Contact("Paul Nyishar", "www.pantech.org", "pantech@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList()
+        );
+    }
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+
+    }
+}
